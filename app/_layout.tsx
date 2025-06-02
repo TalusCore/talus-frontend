a28860576fbce6ca4f62c5142540a806ab80b89e
Nelson Loop <nelsonploop@gmail.com>
2025-06-02 00:01:18 -0400

    Fixed errors so check will pass.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,5 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return <Stack />;
 }
-
-// Editing to trigger the lint workflow
