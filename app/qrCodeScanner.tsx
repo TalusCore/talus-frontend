9108529e52e388d9f03bf8bb9b43b90d32ccadf1
Nelson Loop <nelsonploop@gmail.com>
2025-06-03 16:32:06 -0400

    Added warning for invalid Id.

--- app/qrCodeScanner.tsx
+++ app/qrCodeScanner.tsx
@@ -9,20 +9,27 @@ const ID_PREFIX = 'talus-code/';
 
 const handleQrCodeScanned = (
   event: { data: string },
-  setScannedId: (id: string) => void
+  setScannedId: (id: string | null) => void,
+  setValidId: (isValid: boolean) => void
 ): void => {
   const { data } = event;
-  console.log('QR Code scanned:', data);
 
   if (data.startsWith(ID_PREFIX)) {
     const id = data.split(ID_PREFIX)[1];
+    console.log('QR Code scanned:', data);
     setScannedId(id);
+    setValidId(true);
+  } else {
+    console.log('Invalid QR code scanned:', data);
+    setScannedId(null);
+    setValidId(false);
   }
 };
 
 const QrCodeScanner = (): React.JSX.Element => {
   const [permission, requestPermission] = useCameraPermissions();
   const [scannedId, setScannedId] = useState<string | null>(null);
+  const [validId, setValidId] = useState<boolean>(true);
 
   if (!permission) {
     return (
@@ -49,12 +56,32 @@ const QrCodeScanner = (): React.JSX.Element => {
     );
   }
 
+  if (!validId) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Invalid QR code scanned</Text>
+        <Button
+          mode="contained"
+          icon="camera"
+          onPress={() => {
+            setValidId(true);
+          }}
+          style={{ marginTop: 20 }}
+        >
+          Scan Again
+        </Button>
+      </View>
+    );
+  }
+
   if (scannedId == null) {
     return (
       <CameraView
         style={{ flex: 1 }}
         facing="back"
-        onBarcodeScanned={event => handleQrCodeScanned(event, setScannedId)}
+        onBarcodeScanned={event =>
+          handleQrCodeScanned(event, setScannedId, setValidId)
+        }
         barcodeScannerSettings={{
           barcodeTypes: ['qr']
         }}
