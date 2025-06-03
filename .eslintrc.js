3d3dc46cac192ebc140328cbdc0a2f3fc206b108
Nelson Loop <nelsonploop@gmail.com>
2025-06-03 11:58:57 -0400

    Fix formatting issues and uninstalled MUI.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,59 @@
 module.exports = {
-  extends: ['expo', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'expo',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
   plugins: [
     '@typescript-eslint',
     'react',
     'react-native',
     'react-hooks',
-    'prettier',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
   },
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   rules: {
     'prettier/prettier': 'error',
     'react-native/no-inline-styles': 'off',
     'react-native/no-color-literals': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    'comma-dangle': ['error', 'never'],
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: false,
+        allowTypedFunctionExpressions: true
+      }
+    ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/strict-boolean-expressions': 'error'
   },
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
-  ignorePatterns: ['dist/*', 'node_modules/*', 'expo-env.d.ts'],
+  ignorePatterns: ['dist/*', 'node_modules/*', 'expo-env.d.ts', '.eslintrc.js'],
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],
-      parser: '@typescript-eslint/parser',
-    },
-  ],
+      parser: '@typescript-eslint/parser'
+    }
+  ]
 };
