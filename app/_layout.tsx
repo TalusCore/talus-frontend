05cd3f0727629c091c9788c399637e9e6fd28550
Nelson Loop <97408852+NelsonL2903@users.noreply.github.com>
2025-06-04 13:26:53 -0400

    Create app skeleton (#2)
    * Installed MUI.
    
    * Fix formatting issues and uninstalled MUI.
    
    * Created empty home and settings pages.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,36 @@
+import SettingsButton from '@/components/settingsButton';
 import { Stack } from 'expo-router';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const RootLayout = (): React.JSX.Element => {
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            headerRight: SettingsButton,
+            headerTintColor: '#fff',
+            headerStyle: {
+              backgroundColor: '#25292e'
+            }
+          }}
+        />
+        <Stack.Screen
+          name="settings"
+          options={{
+            title: 'Settings',
+            headerTintColor: '#fff',
+            headerStyle: {
+              backgroundColor: '#25292e'
+            }
+          }}
+        />
+      </Stack>
+    </SafeAreaProvider>
+  );
+};
+
+export default RootLayout;
