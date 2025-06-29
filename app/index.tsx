2061e80a52c87266d3b137279ba0e7f042672772
Nelson Loop <nelsonploop@gmail.com>
2025-06-29 16:59:19 -0400

    Connected to signup and login apis.

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,104 @@
+import { login } from '@/api/userApi';
 import styles from '@/components/styles';
+import { capitalizeFirstLetter } from '@/components/utils';
+import { router } from 'expo-router';
+import { useState } from 'react';
 import { Text, View } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
 
-const Home = (): React.JSX.Element => {
+const Login = (): React.JSX.Element => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = async (): Promise<void> => {
+    let hasError = false;
+
+    if (!email.trim()) {
+      setEmailError(true);
+      hasError = true;
+    } else {
+      setEmailError(false);
+    }
+
+    if (!password.trim()) {
+      setPasswordError(true);
+      hasError = true;
+    } else {
+      setPasswordError(false);
+    }
+
+    if (!hasError) {
+      const loginData = await login(email, password);
+
+      if (loginData.success) {
+        console.log('Login successful:', loginData);
+        router.replace('/home');
+      } else {
+        const errorMsg = capitalizeFirstLetter(
+          loginData.error,
+          'Invalid email or password. Please try again.'
+        );
+        setErrorMessage(errorMsg);
+      }
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is the home page</Text>
+    <View style={{ ...styles.container, justifyContent: 'space-evenly' }}>
+      <View style={{ width: '100%', alignItems: 'center' }}>
+        <Text style={styles.text}>Please sign in to continue.</Text>
+        <TextInput
+          label="Email"
+          mode="flat"
+          style={{ width: '80%', marginTop: 20 }}
+          autoCapitalize="none"
+          keyboardType="email-address"
+          autoComplete="email"
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={setEmail}
+          error={emailError}
+        />
+        <TextInput
+          label="Password"
+          mode="flat"
+          style={{ width: '80%', marginTop: 20 }}
+          secureTextEntry
+          placeholder="Enter your password"
+          value={password}
+          onChangeText={setPassword}
+          error={passwordError}
+        />
+        {errorMessage ? (
+          <Text style={{ color: 'red', marginTop: 20 }}>{errorMessage}</Text>
+        ) : null}
+        <Button
+          mode="contained"
+          icon="login"
+          onPress={handleLogin}
+          style={{ marginTop: 20 }}
+        >
+          Sign In
+        </Button>
+      </View>
+      <View style={{ width: '100%', alignItems: 'center' }}>
+        <Text style={styles.text}>Don't have an account?</Text>
+        <Button
+          mode="contained"
+          icon="account-plus"
+          onPress={() => {
+            router.push('/signup');
+          }}
+          style={{ marginTop: 10 }}
+        >
+          Sign Up Here
+        </Button>
+      </View>
     </View>
   );
 };
 
-export default Home;
+export default Login;
---
+++ app/signup.tsx
@@ -0,0 +1,109 @@
+import { signup } from '@/api/userApi';
+import styles from '@/components/styles';
+import { capitalizeFirstLetter } from '@/components/utils';
+import { router } from 'expo-router';
+import { useState } from 'react';
+import { Text, View } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
+
+const SignUp = (): React.JSX.Element => {
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSignUp = async (): Promise<void> => {
+    let hasError = false;
+
+    if (!firstName.trim()) {
+      setFirstNameError(true);
+      hasError = true;
+    } else {
+      setFirstNameError(false);
+    }
+
+    if (!email.trim()) {
+      setEmailError(true);
+      hasError = true;
+    } else {
+      setEmailError(false);
+    }
+
+    if (!password.trim()) {
+      setPasswordError(true);
+      hasError = true;
+    } else {
+      setPasswordError(false);
+    }
+
+    if (!hasError) {
+      const signUpData = await signup(firstName, email, password);
+
+      if (signUpData.success) {
+        console.log('Sign up successful:', signUpData);
+        router.replace('/home');
+      } else {
+        const errorMsg = capitalizeFirstLetter(
+          signUpData.error,
+          'Ensure that your sign up data is correct.'
+        );
+        setErrorMessage(errorMsg);
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Please sign up to continue.</Text>
+      <TextInput
+        label="First Name"
+        mode="flat"
+        style={{ width: '80%', marginTop: 20 }}
+        autoCapitalize="words"
+        placeholder="Enter your first name"
+        autoComplete="given-name"
+        value={firstName}
+        onChangeText={setFirstName}
+        error={firstNameError}
+      />
+      <TextInput
+        label="Email"
+        mode="flat"
+        style={{ width: '80%', marginTop: 20 }}
+        autoCapitalize="none"
+        keyboardType="email-address"
+        autoComplete="email"
+        placeholder="Enter your email"
+        value={email}
+        onChangeText={setEmail}
+        error={emailError}
+      />
+      <TextInput
+        label="Password"
+        mode="flat"
+        style={{ width: '80%', marginTop: 20 }}
+        secureTextEntry
+        placeholder="Enter your password"
+        value={password}
+        onChangeText={setPassword}
+        error={passwordError}
+      />
+      {errorMessage ? (
+        <Text style={{ color: 'red', marginTop: 20 }}>{errorMessage}</Text>
+      ) : null}
+      <Button
+        mode="contained"
+        icon="account-plus"
+        onPress={handleSignUp}
+        style={{ marginTop: 20 }}
+      >
+        Sign Up
+      </Button>
+    </View>
+  );
+};
+
+export default SignUp;
