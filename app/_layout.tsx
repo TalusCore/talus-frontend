7edf49de231314462bf196b8522eba9b0d00b7e9
Nelson Loop <nelsonploop@gmail.com>
2025-06-30 02:07:15 -0400

    Added auth context and connected the pair talus api.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,65 +1,68 @@
 import SettingsButton from '@/components/settingsButton';
+import { AuthProvider } from '@/contexts/AuthContext';
 import { Stack } from 'expo-router';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const RootLayout = (): React.JSX.Element => {
   return (
-    <SafeAreaProvider>
-      <Stack initialRouteName="index">
-        <Stack.Screen
-          name="index"
-          options={{
-            title: 'Login',
-            headerTintColor: '#fff',
-            headerStyle: {
-              backgroundColor: '#25292e'
-            }
-          }}
-        />
-        <Stack.Screen
-          name="signup"
-          options={{
-            title: 'Sign Up',
-            headerTintColor: '#fff',
-            headerStyle: {
-              backgroundColor: '#25292e'
-            }
-          }}
-        />
-        <Stack.Screen
-          name="home"
-          options={{
-            title: 'Home',
-            headerRight: SettingsButton,
-            headerTintColor: '#fff',
-            headerStyle: {
-              backgroundColor: '#25292e'
-            }
-          }}
-        />
-        <Stack.Screen
-          name="settings"
-          options={{
-            title: 'Settings',
-            headerTintColor: '#fff',
-            headerStyle: {
-              backgroundColor: '#25292e'
-            }
-          }}
-        />
-        <Stack.Screen
-          name="qrCodeScanner"
-          options={{
-            title: 'QR Code Scanner',
-            headerTintColor: '#fff',
-            headerStyle: {
-              backgroundColor: '#25292e'
-            }
-          }}
-        />
-      </Stack>
-    </SafeAreaProvider>
+    <AuthProvider>
+      <SafeAreaProvider>
+        <Stack initialRouteName="index">
+          <Stack.Screen
+            name="index"
+            options={{
+              title: 'Login',
+              headerTintColor: '#fff',
+              headerStyle: {
+                backgroundColor: '#25292e'
+              }
+            }}
+          />
+          <Stack.Screen
+            name="signup"
+            options={{
+              title: 'Sign Up',
+              headerTintColor: '#fff',
+              headerStyle: {
+                backgroundColor: '#25292e'
+              }
+            }}
+          />
+          <Stack.Screen
+            name="home"
+            options={{
+              title: 'Home',
+              headerRight: SettingsButton,
+              headerTintColor: '#fff',
+              headerStyle: {
+                backgroundColor: '#25292e'
+              }
+            }}
+          />
+          <Stack.Screen
+            name="settings"
+            options={{
+              title: 'Settings',
+              headerTintColor: '#fff',
+              headerStyle: {
+                backgroundColor: '#25292e'
+              }
+            }}
+          />
+          <Stack.Screen
+            name="qrCodeScanner"
+            options={{
+              title: 'QR Code Scanner',
+              headerTintColor: '#fff',
+              headerStyle: {
+                backgroundColor: '#25292e'
+              }
+            }}
+          />
+        </Stack>
+      </SafeAreaProvider>
+    </AuthProvider>
   );
 };
 
